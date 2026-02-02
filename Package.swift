bddef514b9c259997af52d51503d59cf38cd12e6
CI/Damien <damien.Walerowicz@miam.tech>
2026-02-02 17:13:22 +0100

    iOS release 0.1.0-alpha.96

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MealzUIKit",
-            url: "https://github.com/miamtech/MealzUIKitRelease/raw/refs/tags/0.1.0-alpha.95/MealzUIKit.zip",
-            checksum: "efe9a49f94410e1a9b52e3b980578ff55e01f9626f64b5ba55b73b17e547c5c2"
+            url: "https://github.com/miamtech/MealzUIKitRelease/raw/refs/tags/0.1.0-alpha.96/MealzUIKit.zip",
+            checksum: "0689c02ffdd6be645a6d298c135796a3bf974fc559a5edf9afbb1e1c26f019bb"
         )
     ]
 )
